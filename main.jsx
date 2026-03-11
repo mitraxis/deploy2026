1953f791fbb8a12c9f21a649bb150318685f8c74
Your Name <your@email.com>
2026-03-11 08:19:51 +0100

    project-setup

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SoloProtocol from './solo-deployment-protocol.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SoloProtocol />
+  </React.StrictMode>,
+)
